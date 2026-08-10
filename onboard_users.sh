f58cf1117fd70884272ff2c2e6174414781d3d58
renaskitamura <105678722+renaskitamura@users.noreply.github.com>
2026-08-09 23:19:23 -0400

    Enhance UX: Add AccountChooser link, styled password box, and optimize HTML rendering

--- onboard_users.sh
+++ onboard_users.sh
@@ -43,21 +43,26 @@ do
     temp_password=$(openssl rand -base64 12) # Generates a random secure password
 
     echo "Processing user: $new_email..."
+    temp_password=$(openssl rand -base64 12)
 
     # 1. Create the user and tell Google to send the password reset link
-    gam create user "$new_email" firstname "$first_name" lastname "$last_name" password "$temp_password" changepassword on notify "$personal_email"
+    gam create user "$new_email" firstname "$first_name" lastname "$last_name" password "$temp_password" changepassword on
 
     # 2. Add to Google Group
     gam update group "$group_name" add member user "$new_email"
 
-    # 3. Read the template and replace the placeholders with the real data
-    EMAIL_BODY=$(sed -e "s/{{FIRST_NAME}}/$first_name/g" \
-                     -e "s/{{EMAIL}}/$new_email/g" \
-                     "$TEMPLATE_FILE")
+    # 3. Replace placeholders and save to a temporary file (Using | to avoid password character conflicts)
+    sed -e "s|{{FIRST_NAME}}|$first_name|g" \
+        -e "s|{{EMAIL}}|$new_email|g" \
+        -e "s|{{PASSWORD}}|$temp_password|g" \
+        "$TEMPLATE_FILE" > temp_email.html
 
-    # 4. Send your custom welcome email
+    # 4. Tell GAM to read the email body directly from the temporary file
     echo "Sending welcome email to $personal_email..."
-    gam user "$ADMIN_EMAIL" sendemail recipient "$personal_email" subject "Welcome to Take Trips MES - Login Instructions" html "$EMAIL_BODY"
+    gam user "$ADMIN_EMAIL" sendemail recipient "$personal_email" subject "Welcome to Take Trips MES - Login Instructions" htmlfile temp_email.html
+    
+    # Clean up the temporary file so it doesn't clutter your folder
+    rm temp_email.html
 
     # 5. Log the action
     echo "$(date '+%Y-%m-%d %H:%M:%S') - CREATED: $new_email | ADDED TO: $group_name | EMAILED: $personal_email" >> "$LOG_FILE"
